6d23268a70b20a16224e92a90f4857e43123ae90
marquigi <markluigi3@yahoo.it>
2025-10-21 17:24:29 +0200

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -9,5 +10,38 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('Dipendenti');
-  // const r: XMLHttpRequest
+
+
+  constructor() {
+
+    // OBSETVABLE:
+    // NOTA
+
+    // NOTA
+    const obs = new Observable(sub => {
+      sub.next(1)
+      sub.next(2)
+      setTimeout(() => {
+        sub.next(3)
+        sub.complete()
+        console.log("Obsevable completato!")
+      }, 2000);
+    })
+
+    // NOTA
+    obs.subscribe({
+      // Si attiva ogni volta che l'obsevable invoca next()
+      next: (x) => {
+        console.log(`ho ricevuto ${x}`)
+      },
+      // Si attiva solo quando l'obsevable invoca error()
+      error: () => {
+        console.log("L'obsevable è andato in errore!")
+      },
+      // Si attiva quando l'observable termina
+      complete: () => {
+        console.log("Ho finito di osservare")
+      }
+    })
+  }
 }
